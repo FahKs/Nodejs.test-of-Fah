a4111c9da9eeb18371491d8ac74de5549dd8c968
FahKs <fahks2021us@gmail.com>
2024-03-12 18:13:10 +0700

    Summary

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const port = 3000;
 const chalk = require('chalk');
 const debug = require('debug');
 const morgan = require('morgan');
+const path = require('path');
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/Public/")));
 
 app.get("/",(req,res)=>{
     res.send('KaningCGM48');
 })
 
 app.listen(port,()=>{
-    debug("Listening on port"+ chalk.green(port));
-})
+    debug("Listening on port",port);
+}) 
